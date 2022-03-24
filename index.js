fd9f29dff2936e73dfc3f4736f1a5c462e7936be
Zeliwipin <zeliwipin@gmail.com>
2022-03-24 13:59:43 +0100

    started to create the step neededs for deployment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import packacages
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const formidableMiddleware = require("express-formidable");
 
 //import the models
 const KnowledgeNugget = require("./Models/knowledgenugget");
@@ -8,28 +10,58 @@ const KnowledgeNugget = require("./Models/knowledgenugget");
 //initialise express
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/wikimathcomputer");
+//initialise cors
+app.use(cors());
+//initialise formidable Middleware
+app.use(formidableMiddleware());
+//initialse dotenv
+require("dotenv").config(); // Permet d'activer les variables d'environnement qui se trouvent dans le fichier `.env`
+
+mongoose.connect(process.env.MONGODB_URI);
 
 app.get("/", (req, res) => {
   console.log(KnowledgeNugget);
   res.json({ message: "working" });
 });
 
-app.post("/create", async (req, res) => {
-  console.log("route : /create");
+app.get("/getinitialknowledge", async (req, res) => {
   try {
-    const newKnowledgeNugget = new KnowledgeNugget({
-      name: "combinatorics",
-      definition:
-        "the branch of mathematics concerned with the number of different arrangements of given objects subject to various restrictions",
-    });
-    await newKnowledgeNugget.save();
-
-    res.json({ message: "new knowledgenugget created successfully" });
+    const initialKnowledgeNugget = await KnowledgeNugget.find().limit(4);
+    res.json(initialKnowledgeNugget);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
+
+app.post("/specificknowledge", async (req, res) => {
+  try {
+    const s = req.query.name;
+    const regex = new RegExp(s, "i");
+    console.log(req.query.name);
+    const initialKnowledgeNugget = await KnowledgeNugget.find({
+      name: { $regex: regex },
+    });
+    res.status(200).json(initialKnowledgeNugget);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// app.post("/create", async (req, res) => {
+//   console.log("route : /create");
+//   try {
+//     const newKnowledgeNugget = new KnowledgeNugget({
+//       name: "combinatorics",
+//       definition:
+//         "the branch of mathematics concerned with the number of different arrangements of given objects subject to various restrictions",
+//     });
+//     await newKnowledgeNugget.save();
+
+//     res.json({ message: "new knowledgenugget created successfully" });
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 
 app.listen(8000, function () {
   console.log("now working");
